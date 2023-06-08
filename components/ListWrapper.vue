79f04ad1d091a520961285b63beeb79ea380af2f
Igor Petrov <petrovia93@mail.ru>
2023-06-08 17:17:38 +1200

    new AD system

--- components/ListWrapper.vue
+++ components/ListWrapper.vue
@@ -15,20 +15,8 @@
       <div class="item-wrapper" v-for="(job, index) in list" :key="index">
         <component :is="templateMobile" :job="job" :index="index+1"/>
 
-        <GptAd v-if="(index+1) % 3 === 0"
-               ad-unit="lineup"
-               :collapseEmptyDiv="true"
-               :size="[300, 250]"
-        />
-
-        <GptAd v-if="(index+1) % 4 === 0"
-               ad-unit="22937936231/lineup"
-               :collapseEmptyDiv="true"
-               :size="[300, 250]"
-        />
-
         <GptAd v-if="(index+1) % 5 === 0"
-               ad-unit="/22641207500/lineup"
+               ad-unit="lineup"
                :collapseEmptyDiv="true"
                :size="[300, 250]"
         />
